1134294fd4fcda9072262c513f44ed02089f0ea9
Betoside <betoside@gmail.com>
2021-08-05 22:58:25 -0300

    tryCat email, politica de privacidade, ssl

--- form-action.php
+++ form-action.php
@@ -2,21 +2,25 @@
 
 if (isset($_POST['nome']) && !empty($_POST['nome'])) {
 
-    $assunto = addslashes($_POST['assunto']); // fale, carreira, imprensa
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);   
-    $empresa = addslashes($_POST['empresa']);
-    $telefone = addslashes($_POST['telefone']);
-    $mensagem = addslashes($_POST['mensagem']);
-    $assinar = addslashes($_POST['assinar']);
+    $assunto = addslashes($_POST['assunto']); // Fale Conosco, Carreira, Imprensa
+    if ($assunto == 'Fale Conosco') {
+        $email_to = "contato@peckadv.com.br";
+    }
+    if ($assunto == 'Carreira') {
+        $email_to = "carreira@peckadv.com.br";
+    }
+    if ($assunto == 'Imprensa') {
+        $email_to = "imprensa@peckadv.com.br";
+    }
 
-    
-    $assunto = addslashes($_POST['assunto']); // fale, carreira, imprensa
     $nome = addslashes($_POST['nome']);
     $email = addslashes($_POST['email']);   
     $empresa = addslashes($_POST['empresa']);
     $telefone = addslashes($_POST['telefone']);
+
     $mensagem = addslashes($_POST['mensagem']);
+    $mensagem = nl2br($mensagem, false);
+
     $assinar = addslashes($_POST['assinar']);
     if(!empty($assinar)){
         $assinar = "Sim, desejo receber newsletter";
@@ -25,17 +29,41 @@ if (isset($_POST['nome']) && !empty($_POST['nome'])) {
     }
 
     $preview = "";
-    
-    
-    $preview =  '$assunto: '.$assunto. "<br>" .
-                '$nome: '.$nome. "<br>" .
-                '$email: '.$email. "<br>" .
-                '$empresa: '.$empresa. "<br>" .
-                '$telefone: '.$telefone. "<br>" .
-                '$Newsletter: '.$assinar. "<br>" .
-                '$mensagem: '.$mensagem;
+    $preview =  '<strong>Assunto</strong>: '.$assunto. "<br><br>"   .
+                '<strong>Nome</strong>: '.$nome. "<br><br>" .
+                '<strong>Email</strong>: '.$email. "<br><br>" .
+                '<strong>Email Para</strong>: '.$email_to. "<br><br>" .
+                '<strong>Empresa</strong>: '.$empresa. "<br><br>" .
+                '<strong>Telefone</strong>: '.$telefone. "<br><br>" .
+                '<strong>Newsletter</strong>: '.$assinar. "<br><br>" .
+                '<strong>Mensagem</strong>: <br>'.$mensagem;
+    // echo $preview;
+    // exit;
 
-    echo $preview;
+    $msg =  '<strong>Assunto</strong>: '.$assunto. "<br><br>"   .
+                '<strong>Nome</strong>: '.$nome. "<br><br>" .
+                '<strong>Email</strong>: '.$email. "<br><br>" .
+                '<strong>Empresa</strong>: '.$empresa. "<br><br>" .
+                '<strong>Telefone</strong>: '.$telefone. "<br><br>" .
+                '<strong>Newsletter</strong>: '.$assinar. "<br><br>" .
+                '<strong>Mensagem</strong>: <br>'.$mensagem;
+
+    // contato@peckadv.com.br
+
+    $assunto = "Contato do site: " . $assunto;
+    $headers = "FROM: ".$email."\r\n".
+                "X-Mailer: PHP/".phpversion();
+
+    try {
+        $mail($email_to, $assunto, $msg, $headers);
+        header("Location: index.html?Email_success=1");
+    } catch (\Throwable $th) {
+        header("Location: index.html?Email_Error=1");
+    }
+    // $mail($email_to, $assunto, $msg, $headers);
+    // echo "<h2>Ok! Você receberá um email com o link para confirmar seu cadastro</h2>";
+    // exit;
+
 
 // Quarta feira, 4 de agosto.
 
